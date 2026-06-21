dbbe30d89b2abe28909d27a0f4b8ec208a27b433
ndgkhoa <ngdangkhoa2330@gmail.com>
2026-06-21 22:36:15 +0700

    chore: bump postgres to 18.4 and rabbitmq to 4.3.2

--- test/global-setup.ts
+++ test/global-setup.ts
@@ -11,10 +11,10 @@ import { ENV_FILE, type TestContainerEnv } from '@test/helpers/container-env.js'
  * setup.ts reads inside each worker. Containers are torn down in the returned teardown.
  */
 export default async function setup(): Promise<() => Promise<void>> {
-  const pg = await new PostgreSqlContainer('postgres:18.3').start();
+  const pg = await new PostgreSqlContainer('postgres:18.4').start();
   await runMigrations(pg.getConnectionUri());
 
-  const rabbit = await new GenericContainer('rabbitmq:4.2.5-management')
+  const rabbit = await new GenericContainer('rabbitmq:4.3.2-management')
     .withExposedPorts(5672)
     .withWaitStrategy(Wait.forLogMessage('Server startup complete'))
     .withStartupTimeout(120_000)
